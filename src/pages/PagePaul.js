9780ce32bfb1fd1a27cdd613760f1f30d180bfae
Paul Mouliac <paulmouliac@pauls-mbp.home>
2024-04-29 22:26:17 +0200

    Ajout du Header

--- src/pages/PagePaul.js
+++ src/pages/PagePaul.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Header from "../components/header"; // Make sure the path is correct.
 
 function PagePaul() {
     return (
-        <div>Paul
+        <div>
+            <Header /> 
+            <div>Paul
+            </div>
         </div>
     );
 }
